f134f426e338c00f388e5be2eb954a0a6a545881
Lars Janssen <larsjanssen64@gmail.com>
2018-03-01 14:22:59 +0100

    Logic changes

--- src/Controllers/AuthenticationController.php
+++ src/Controllers/AuthenticationController.php
@@ -3,43 +3,45 @@
 namespace LarsJanssen\ChatbotAuthentication\Controllers;
 
 use BotMan\BotMan\BotMan;
-use BotMan\BotMan\BotManFactory;
 use Illuminate\Routing\Controller;
-use BotMan\BotMan\Drivers\DriverManager;
-use BotMan\Drivers\Web\WebDriver;
+use LarsJanssen\ChatbotAuthentication\Traits\InitDriver;
 
 class AuthenticationController extends Controller
 {
+    use InitDriver;
+
     /**
-     *
+     * Show login form.
      */
     public function login()
     {
         return view('views::login');
     }
 
-    public function test()
+    /**
+     * Return response based on given input.
+     */
+    public function response()
     {
-        $config = [
-            'web' => [
-                'matchingData' => [
-                    'driver' => 'web',
-                ],
-            ]
-        ];
-
-        // Load the driver(s) you want to use
-        DriverManager::loadDriver(WebDriver::class);
-
-        // Create an instance
-        $botman = BotManFactory::create($config);
-
         // Give the bot something to listen for.
-        $botman->hears('hello', function (BotMan $bot) {
+        $this->botman->hears('hello', function (BotMan $bot) {
             $bot->reply('Hello yourself.');
         });
 
         // Start listening
-        $botman->listen();
+        $this->botman->listen();
+    }
+
+    /**
+     * Return welcome message based
+     * on config file.
+     */
+    public function welcome()
+    {
+        $this->botman->hears('hi', function (BotMan $bot) {
+            $bot->reply("welcome", ["hi there!"]);
+        });
+
+        $this->botman->listen();
     }
 }
